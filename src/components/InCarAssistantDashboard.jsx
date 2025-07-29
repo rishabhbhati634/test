c729fba6cb717aee5828c91d3a25c33eaaa5d1b0
rishabh bhati <165822970+rishabhbhati634@users.noreply.github.com>
2025-07-29 18:52:42 +0530

    Update InCarAssistantDashboard.jsx

--- src/components/InCarAssistantDashboard.jsx
+++ src/components/InCarAssistantDashboard.jsx
@@ -80,7 +80,7 @@ const InCarAssistantDashboard = () => {
     setInputText("");
     setIsAssistantTyping(true);
     try {
-      const response = await fetch("http://localhost:5000/api/openai/translate", {
+      const response = await fetch("https://tata-innovent-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
